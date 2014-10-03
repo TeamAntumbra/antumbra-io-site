e2c0a8bce56b1e0b07026a9e0ca9131c0b623734
Adam Walsh <adamwalsh14@gmail.com>
2014-10-02 20:18:11 -0400

    Added FAQ section and content

--- js/theme.js
+++ js/theme.js
@@ -28,3 +28,21 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function() {
+
+    $('.faq_question').click(function() {
+
+        if ($(this).parent().is('.open')){
+            $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
+            $(this).closest('.faq').removeClass('open');
+
+            }else{
+                var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
+                $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);
+                $(this).closest('.faq').addClass('open');
+            }
+
+    });
+
+});

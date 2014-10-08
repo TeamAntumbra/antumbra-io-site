08ea8b76c26b57ee080d5885b73795ea9acbed1d
Adam Walsh <adamwalsh14@gmail.com>
2014-10-08 00:02:48 -0400

    Added carousel, and updated font awesome

--- js/theme.js
+++ js/theme.js
@@ -46,3 +46,15 @@ $(document).ready(function() {
     });
 
 });
+
+
+
+$(document).ready(function() {
+     $("#carousel").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        navigationText : ["<-","->"],
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+});

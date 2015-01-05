e341105c867feca690eba6caac514ee7d28f8103
Adam Walsh <adamwalsh14@gmail.com>
2015-01-05 15:07:12 -0500

    Added timeline resources locally, converted css theming to dark, removed un-used helper, minor clean ups

--- config.rb
+++ config.rb
@@ -39,14 +39,6 @@ page "/blog/*", :layout => "blog"
 # Helpers
 ###
 # Methods defined in the helpers block are available in templates
-helpers do
-    # Returns all pages under a certain directory.
-    def sub_pages(dir)
-        sitemap.resources.select do |resource|
-            resource.path.start_with?(dir)
-        end
-    end
-end
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes

1c048d9f8160ed7768513f850aec6dab7459b293
Adam Walsh <adamwalsh14@gmail.com>
2015-04-04 18:29:00 -0400

    Cleaned out un-needed boilerplate comments

--- config.rb
+++ config.rb
@@ -1,13 +1,4 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 #HAML
 ###
 set :haml, { :ugly => true, :format => :html5 }
@@ -15,36 +6,12 @@ set :haml, { :ugly => true, :format => :html5 }
 ###
 # Page options, layouts, aliases and proxies
 ###
-
-# Per-page layout changes:
 page "", :layout => "base"
 page "/blog.html", :layout => "base"
 page "/blog/*", :layout => "blog"
 page "/beta.html", :layout => "base"
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-# Methods defined in the helpers block are available in templates
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
+# Development-specific configuration
 configure :development do
   activate :disqus do |d|
       d.shortname = 'antumbra-development'
@@ -88,16 +55,4 @@ configure :build do
   end
 
   activate :directory_indexes
-
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # activate :relative_links
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
